ead0393ca003ab6f45e3796704423fdcc0944259
Derek Wolpert <me@derekwolpert.com>
2020-06-15 16:43:22 -0400

    limit output for webpack build
    Former-commit-id: 5758efe97161a58b5a91d67eded4f8e6d1213ad2 [formerly c401bc58b7536892d0542c7cc21d1faf94aa13d2]
    Former-commit-id: ef6d7d948c3a7b16d0172676faebe1f8684b230b

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -4,6 +4,7 @@ const common = require("./webpack.common.js");
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(common, {
+    stats: "errors-only",
     entry: path.resolve(__dirname, "src", "main.js"),
     mode: "production",
     plugins: [
