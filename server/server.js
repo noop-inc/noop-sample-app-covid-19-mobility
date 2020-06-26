d566c339545e5fd7a264f06b1185cee0597aff5f
Derek Wolpert <me@derekwolpert.com>
2020-06-26 16:48:25 -0400

    add  endpoint to api to return random data entry,ato be used to relay data on homepage in vue frontend
    Former-commit-id: 3466a695ee1b43b4774ccfac73a48ae1866ba9f8

--- server/server.js
+++ server/server.js
@@ -1,43 +1,14 @@
 const express = require("express");
 const app = express();
-const AWS = require("aws-sdk");
 const compression = require("compression");
 const morgan = require("morgan");
-
-const TableName = process.env.DYNAMO_TABLE;
-const Endpoint = process.env.DYNAMO_ENDPOINT;
-
-AWS.config.update({ endpoint: Endpoint });
-
-const dynamodb = new AWS.DynamoDB();
-const documentClient = new AWS.DynamoDB.DocumentClient({
-    service: dynamodb,
-});
+const api = require("./routes/api");
 
 app.use(compression({ level: 9 }));
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.get("/api/:name/:type", (req, res) => {
-    const name = req.params.name;
-    const type = req.params.type;
-    const params = {
-        TableName,
-        Key: {
-            name,
-            type,
-        },
-    };
-    documentClient.get(params, (err, data) => {
-        if (err) {
-            return res.status(400).json({ error: err });
-        } else if (data.Item) {
-            return res.json(data.Item);
-        } else {
-            return res.status(404).json({ error: "Data could not be found" });
-        }
-    });
-});
+app.use("/api", api);
 
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
