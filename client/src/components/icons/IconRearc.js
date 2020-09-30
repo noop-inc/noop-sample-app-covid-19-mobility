a03d204eb01e616faed23aa7ce6ca9a9f212a230
Derek Wolpert <me@derekwolpert.com>
2020-09-30 13:49:56 -0400

    update noop logo in header navbar

--- client/src/components/icons/IconRearc.js
+++ client/src/components/icons/IconRearc.js
@@ -5,11 +5,206 @@ export default {
       <svg
         xmlns='http://www.w3.org/2000/svg'
         height='24px'
-        viewBox='0 0 2300 2300'
+        viewBox='0 0 32 32'
         preserveAspectRatio='xMidYMid meet'
       >
-        <g fill='currentColor'>
-          <path d='M1050 2294 c-259 -37 -420 -101 -595 -233 -314 -239 -488 -637 -446 -1021 29 -274 141 -511 330 -700 314 -314 762 -417 1187 -275 476 160 802 648 770 1152 -24 381 -237 727 -568 923 -192 115 -486 181 -678 154z m45 -741 l246 -237 -30 -29 -30 -30 -148 141 c-81 77 -197 190 -256 249 l-108 109 21 27 c17 22 25 25 40 17 10 -5 129 -116 265 -247z m-26 -283 c137 -132 252 -244 255 -249 3 -4 -8 -21 -24 -36 l-29 -28 -158 151 c-87 83 -202 195 -256 249 l-98 99 21 27 c12 15 25 27 30 27 5 0 121 -108 259 -240z m-436 113 c51 -48 166 -159 256 -246 l164 -158 -31 -31 -31 -31 -255 248 c-141 136 -256 250 -256 253 0 10 43 52 53 52 5 0 50 -39 100 -87z m936 -405 c10 -62 18 -88 28 -88 7 0 45 16 83 35 38 19 73 35 78 35 8 0 62 -90 62 -104 0 -3 -36 -24 -79 -46 -44 -22 -82 -44 -85 -49 -4 -5 25 -38 64 -73 l69 -63 -47 -48 -46 -47 -68 67 c-37 37 -69 65 -72 62 -2 -2 -20 -35 -39 -74 -54 -105 -47 -101 -106 -70 -59 29 -59 26 -10 129 16 34 29 66 29 72 0 6 -41 16 -92 23 -51 6 -93 13 -95 15 -1 1 2 30 8 64 l11 62 52 -6 c28 -4 71 -10 95 -14 l44 -8 -7 52 c-21 162 -22 154 10 159 16 2 45 4 64 3 l35 -1 14 -87z' />
+        <g fill='currentColor' stroke='currentColor'>
+          <path
+            d='M26.37 4.75a30.993 30.993 0 0 0-20.52 0'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M5.86 4.74a14.473 14.473 0 0 0-3.73 6.46'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M2.12 11.2c1.3 6.92 4.92 13.18 10.26 17.76'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M12.38 28.96c2.44.65 5.01.65 7.46 0'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M19.85 28.96A30.942 30.942 0 0 0 30.11 11.2'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M30.1 11.2c-.66-2.44-1.94-4.67-3.73-6.46'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M26.84 3.99a31.76 31.76 0 0 0-21.46 0'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M5.38 4a15.335 15.335 0 0 0-4.14 7.17'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M1.24 11.16a31.762 31.762 0 0 0 10.73 18.58'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M11.97 29.74c2.71.76 5.58.76 8.29 0'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M20.26 29.75a31.814 31.814 0 0 0 10.73-18.58'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M30.99 11.17c-.7-2.72-2.13-5.2-4.14-7.17'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+          />
+          <path
+            d='M25.22 22.96c1.07-4.94-2-9.84-6.91-11.03'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M18.31 11.93c-.19-.05-.39 0-.54.12a.63.63 0 0 0-.24.49'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M13.92 11.94C9 13.13 5.93 18.02 7.01 22.96'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M14.68 12.54a.63.63 0 0 0-.24-.49c-.15-.12-.35-.16-.54-.12'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M14.7 18.8v-6.26'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M13.5 20.85c.73-.42 1.18-1.21 1.18-2.05'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M17.53 18.8v-6.26'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M9.68 26.34l5.25-3.02'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M17.3 23.32c-.73-.42-1.64-.42-2.37 0'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M7.84 24.13l5.67-3.27'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M24.39 24.13l-5.67-3.27'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M17.53 18.79c0 .85.45 1.63 1.18 2.05'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M22.54 26.34l-5.25-3.02'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
+          <path
+            d='M28.94 15.58l-2.16.56-.56-.97-.57-.98 1.99-2.05-1.34-1.34-1.34-1.34-2.03 1.99-.98-.56-.98-.57.7-2.75-1.82-.5-1.84-.48-.76 2.74h-2.26l-.78-2.74-1.82.48-1.84.5.71 2.75-.98.57-.98.56-2.04-1.99-1.34 1.34-1.34 1.34 1.99 2.05-.56.98-.57.97-2.15-.56'
+            fill='none'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            stroke-miterlimit='10'
+            stroke='currentColor'
+          />
         </g>
       </svg>
     )
