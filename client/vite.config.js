4919a171bd1ebba6c1c322bfe4b1ebdc6ef6ce1c
Derek Wolpert <me@derekwolpert.com>
2021-06-28 16:46:52 -0400

    comment out proxy settings in vite.config.js file

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,14 @@ const { createVuePlugin } = require('vite-plugin-vue2')
 
 module.exports = {
   plugins: [createVuePlugin()],
-  server: {
-    proxy: {
-      '^/(api|data)': {
-        target: 'https://localnoop.app:1234',
-        secure: false
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '^/(api|data)': {
+  //       target: 'https://localnoop.app:1234',
+  //       secure: false
+  //     }
+  //   }
+  // },
   build: {
     minify: false,
     brotliSize: false,
