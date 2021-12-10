115639522d55820bb020431fc50f300fadef1230
Derek Wolpert <me@derekwolpert.com>
2021-12-10 15:36:28 -0500

    update vite config to support noop delegate components

--- website-static/vite.config.js
+++ website-static/vite.config.js
@@ -3,6 +3,13 @@ import { createVuePlugin } from 'vite-plugin-vue2'
 
 export default defineConfig({
   plugins: [createVuePlugin()],
+  server: {
+    hmr: {
+      protocol: 'ws',
+      host: 'localhost',
+      port: 3000
+    }
+  },
   build: {
     target: 'esnext',
     minify: false,
