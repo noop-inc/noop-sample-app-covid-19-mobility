3a2ab7beee8a28ab403f4b42349455d771613713
Derek Wolpert <me@derekwolpert.com>
2022-01-04 16:04:07 -0500

    immediate api call on start of api-service

--- api-service/server.js
+++ api-service/server.js
@@ -3,10 +3,21 @@ const app = express()
 const morgan = require('morgan')
 const api = require('./routes/api')
 
-app.use(express.json())
-app.use(morgan('tiny'))
+const AWS = require('aws-sdk')
+const TableName = process.env.DYNAMO_TABLE
+const Endpoint = process.env.DYNAMO_ENDPOINT
+AWS.config.update({ endpoint: Endpoint })
+const dynamodb = new AWS.DynamoDB()
 
-app.use('/api', api)
+;(async () => {
+  const data = await dynamodb.describeTable({ TableName }).promise()
+  console.log(JSON.stringify(data))
 
-const port = process.env.PORT || 80
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+  app.use(express.json())
+  app.use(morgan('tiny'))
+
+  app.use('/api', api)
+
+  const port = process.env.PORT || 80
+  app.listen(port, () => console.log(`Server is running on port ${port}`))
+})()
